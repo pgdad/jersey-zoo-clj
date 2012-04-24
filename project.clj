4c38459874a7efe9e30e801538f4c5d8815d2ce4
pgdad <pgdad@hotmail.com>
2012-04-24 17:11:16 -0400

    new version of jetty

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jersey-zoo-clj "1.0.1"
+(defproject jersey-zoo-clj "1.0.2"
   :description "jersey servlet with zookeeper connection"
   :aot [jerseyzoo.JerseyZooServletContainer] 
   :repl-init jerseyzoo.JerseyZooServletContainer
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [jersey-clj "1.0.1"]
+                 [jersey-clj "1.0.2"]
                  [zookeeper-clj "0.9.2"]]
-  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]])
+  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]])
